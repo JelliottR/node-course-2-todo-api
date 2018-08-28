f55f8b68d0d5cb7eb9900e5ff43e9e66b7ff8a21
Elliott <elliottrecord@gmail.com>
2018-08-28 21:40:27 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -39,45 +39,63 @@ UserSchema.methods.toJSON = function () {
 };
 
 UserSchema.methods.generateAuthToken = function () {
-  const user = this;
-  const access = 'auth';
-  const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens.push({ access, token });
+    user.tokens.push({access, token});
 
     return user.save().then(() => {
         return token;
     });
 };
 
-UserSchema.statics.findByToken = function (token){
+UserSchema.statics.findByToken = function (token) {
     const User = this;
     var decoded;
 
-    try{
+    try {
         decoded = jwt.verify(token, 'abc123');
-    }catch (e) {
+    } catch (e) {
         return Promise.reject('test');
     }
 
     return User.findOne({
-       '_id': decoded._id,
-       'tokens.token': token,
-       'tokens.access': 'auth'
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 
-UserSchema.pre('save', function(next) {
+UserSchema.statics.findByCredentials = function (email, password) {
+    const User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) return Promise.reject();
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+
+UserSchema.pre('save', function (next) {
     const user = this;
 
-    if (user.isModified('password')){
+    if (user.isModified('password')) {
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, res) => {
                 user.password = res;
                 next();
             })
         })
-    }else{
+    } else {
         next();
     }
 
--- server/server.js
+++ server/server.js
@@ -56,8 +56,8 @@ app.patch('/todos/:id', (req, res) => {
     if (!ObjectId.isValid(id)) {
         return res.status(404).send();
     }
-    
-    if (_.isBoolean(body.completed) && body.completed){
+
+    if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
     } else {
         body.completed = false;
@@ -79,11 +79,24 @@ app.post('/users', (req, res) => {
         return user.generateAuthToken();
     }).then((token) => {
         res.header('x-auth', token).send(user);
-    }).catch( e => res.status(400).send(e));
+    }).catch(e => res.status(400).send(e));
 });
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        console.log(e);
+        res.status(400).send();
+    });
 });
 
 app.listen(port, console.log(`Started on port ${port}`));
